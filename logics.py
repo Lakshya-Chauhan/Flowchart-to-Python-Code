a761ff4ab4df49e8817ae0449769548962f57cc5
Legendary <99966559+Legendary3995@users.noreply.github.com>
2023-07-16 22:04:33 +0530

    Update logics.py

--- logics.py
+++ logics.py
@@ -5,9 +5,9 @@ class logics:
     s = list()      #start/stop
     def __init__(self, type:str):
         self.type = type[0]
-        eval(f"{type[0]}.append(self)")
         self.id = eval(f"len({type[0]})-1")     #'s' for start/stop, 'r' for rectangle, 'd' for diamond, 'p' for parrallelogram
         self.text = ""
+        eval(f"{type[0]}.append(self)")
     def function(self):
         return eval(f"logics.{self.type}func({self.id})")
     def rfunc(id):
