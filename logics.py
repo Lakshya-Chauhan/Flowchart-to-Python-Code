70853c8be5996e5410c9496aff4e70b59607c34e
Legendary <lakshyathelegend@gmail.com>
2023-07-17 13:54:09 +0530

    Update logics.py

--- logics.py
+++ logics.py
@@ -20,11 +20,26 @@ class logics:
         except NameError: return string
         except: pass
         string = string.replace("to", "")
-        m = ["greater", "larger", "bigger", "is more"]#Other words which can be used for 'more'
-        l = ["lesser", "smaller", "shorter", "is less"]#Other words which can be used for 'less'
-        et = ["equals", "is equal", "is"]#==
-        mt = ["more than", "not less than or equal"]#>
-        lt = ["smaller than", "not more than or equal"]#<
-        mtoet = ["more than or equal", "not less than"]#>=
-        ltoet = ["less than or equal", "not more than"]#<=
+        m = ["greater", "larger", "bigger", "is more", "more"]#Other words which can be used for 'more'
+        l = ["lesser", "smaller", "shorter", "is less", "less"]#Other words which can be used for 'less'
+        et = ["equals", "equal", "is equal", "is", "=="]#==
+        mt = ["more than", "not less than or equal", ">"]#>
+        lt = ["smaller than", "not more than or equal", "<"]#<
+        mtoet = ["more than or equal", "not less than", ">="]#>=
+        ltoet = ["less than or equal", "not more than", "<="]#<=
+        for i in m[:-1]:
+            string = string.replace(i, m[-1])
+        for i in l[:-1]:
+            string = string.replace(i, l[-1])
+        for i in mtoet[:-1]:
+            string = string.replace(i, mtoet[-1])
+        for i in ltoet[:-1]:
+            string = string.replace(i, ltoet[-1])
+        for i in mt[:-1]:
+            string = string.replace(i, mt[-1])
+        for i in lt[:-1]:
+            string = string.replace(i, lt[-1])
+        for i in et[:-1]:
+            string = string.replace(i, et[-1])
+            
         
