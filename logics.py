825d3a58cc30ffdd4600a407e02918210596325e
Legendary <lakshyathelegend@gmail.com>
2023-07-17 13:43:53 +0530

    Update logics.py

--- logics.py
+++ logics.py
@@ -13,4 +13,18 @@ class logics:
     def rfunc(id):
         return
     def dfunc(id):
-        return #to be designed by terry
+        string = logics.d[id]
+        try:
+            if (eval(string)) in [True, False]:
+                return string
+        except NameError: return string
+        except: pass
+        string = string.replace("to", "")
+        m = ["greater", "larger", "bigger", "is more"]#Other words which can be used for 'more'
+        l = ["lesser", "smaller", "shorter", "is less"]#Other words which can be used for 'less'
+        et = ["equals", "is equal", "is"]#==
+        mt = ["more than", "not less than or equal"]#>
+        lt = ["smaller than", "not more than or equal"]#<
+        mtoet = ["more than or equal", "not less than"]#>=
+        ltoet = ["less than or equal", "not more than"]#<=
+        
